89f8a74e591effa6123b44f31872b43b9df6645b
sauravatg <Mrwhitedevilcoder@gmail.com>
2026-04-14 12:14:42 +0530

    certificate

--- app/certificates/page.tsx
+++ app/certificates/page.tsx
@@ -10,15 +10,31 @@ export default function CertificatesPage() {
       <div className="cert-grid">
 
         <div className="cert-card">
-          <img src="Certificates/Tkm Hackathon.jpg" alt="Certificate 1" />
+          <img src="/Certificates/IEDC Startup Summit 2026.jpg" alt="Certificate" />
           <div className="cert-content">
-            <h3>Participated 20 Hour Hackathon</h3>
+            <h3>Participation in IEDC Startup Summit 2026</h3>
+            <p>Issued by Kerala Startup Mission · 2026</p>
+          </div>
+        </div>
+
+        <div className="cert-card">
+          <img src="/Certificates/IEDC District Cluster level Hackathon.jpg" alt="Certificate" />
+          <div className="cert-content">
+            <h3>Participated IEDC District Cluster level Hackathon</h3>
+            <p>Issued by Kerala Startup Mission · 2026</p>
+          </div>
+        </div>
+
+        <div className="cert-card">
+          <img src="/Certificates/Tkm Hackathon.jpg" alt="Certificate" />
+          <div className="cert-content">
+            <h3>Participated 20 Hour Hackathon organized by ISTE TKMCE,</h3>
             <p>ISTE TKMCE · 2025</p>
           </div>
         </div>
 
         <div className="cert-card">
-          <img src="/Certificates/Offenso Cyber workshop.jpg" alt="Certificate 2" />
+          <img src="/Certificates/Offenso Cyber workshop.jpg" alt="Certificate" />
           <div className="cert-content">
             <h3>Cyber Security Workshop</h3>
             <p>Issued by Offenso Hackers Acadamy · 2025</p>
@@ -26,12 +42,31 @@ export default function CertificatesPage() {
         </div>
 
         <div className="cert-card">
-          <img src="/Certificates/UNIX & Linux.jpg" alt="Certificate 3" />
+          <img src="/Certificates/UNIX & Linux.jpg" alt="Certificate" />
           <div className="cert-content">
             <h3>Complete UNIX & Linux OS Fundamentals Training</h3>
             <p>Issued by Infosys · 2025</p>
           </div>
         </div>
+
+        
+        <div className="cert-card">
+          <img src="/Certificates/Participation of Hackathon @IHRD Attingal.jpg" alt="Certificate" />
+          <div className="cert-content">
+            <h3>Participated in Hackathon Organised by IEEE IA/IE/PELS Jt. Chapter,Kerala at College of Engineering Attingal</h3>
+            <p>Issued by IEEE IA/IE/PELS Jt. Chapter,Kerala · 2025</p>
+          </div>
+        </div>
+
+        <div className="cert-card">
+          <img src="/Certificates/YUDHYA25 HACKATHON.jpg" alt="Certificate" />
+          <div className="cert-content">
+            <h3>First Place in the YUDHYA’25 HACKATHON conducted on behalf of YUKTHI x YASYA 25</h3>
+            <p>Issued by μlearn & FOSS Club · 2025</p>
+          </div>
+        </div>
+
+      
 
       </div>
     </section>
