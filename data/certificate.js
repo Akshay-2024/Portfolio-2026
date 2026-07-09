ce5db1e66b685649667918263b46ced9d7c48e75
Akshay S <akshay614325@gmail.com>
2026-07-09 09:43:35 +0530

    Add certificate for CompTIA Network: Well-known Ports
    Added a new certificate entry for 'Well-known Ports & Protocols'.

--- data/certificate.js
+++ data/certificate.js
@@ -1,5 +1,12 @@
 export const certificates = [
   {
+    title: "CompTIA Network+: Well-known Ports & Protocols",
+    issuer: "Infosis Springboard",
+    year: 2026,
+    category: "Course",
+    image: "/Certificates/CompTIA2.jpg",
+  },
+  {
     title: "CompTIA Network+: Network Types",
     issuer: "Infosis Springboard",
     year: 2026,
