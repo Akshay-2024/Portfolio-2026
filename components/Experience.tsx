f2ab6df3583974807560bc832b13a18cb706c5b5
Akshay-2024 <akshay614325@gmail.com>
2026-08-01 11:31:28 +0530

    exp added

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -7,7 +7,29 @@ export default function Experience() {
         WORK<br />EXPERIENCE
       </h2>
 
+       <div className="exp-card">
+          <div className="exp-logo">
+            <img src="/icons/Hult_Prize.jpg" alt="Hult Prize" />
+          </div>
+          <div className="exp-role">Media Lead</div>
+          <h3>Director of Visual Media</h3>
+          <p>
+            Capturing every moment and story of Hult Prize UCEK 📸🎥.
+          </p>
+        </div>
+
       <div className="exp-grid">
+
+       <div className="exp-card">
+          <div className="exp-logo">
+            <img src="/icons/legecy.jpg" alt="Legacy Iedc" />
+          </div>
+          <div className="exp-role">Technical Lead</div>
+          <h3>Technical Direction</h3>
+          <p>
+            Managed technical infrastructure and development workflows for digital projects.
+          </p>
+        </div>
 
         <div className="exp-card">
           <div className="exp-logo">
@@ -20,16 +42,6 @@ export default function Experience() {
           </p>
         </div>
 
-        <div className="exp-card">
-          <div className="exp-logo">
-            <img src="/icons/legecy.jpg" alt="Legacy Iedc" />
-          </div>
-          <div className="exp-role">Technical Lead</div>
-          <h3>Technical Direction</h3>
-          <p>
-            Managed technical infrastructure and development workflows for digital projects.
-          </p>
-        </div>
 
         <div className="exp-card">
           <div className="exp-logo">
