61b73dd9d7456ac49bf726bd59d636843efa2e8c
Akshay-2024 <akshay614325@gmail.com>
2026-08-03 14:56:48 +0530

    certificate added

--- data/certificate.js
+++ data/certificate.js
@@ -6,6 +6,13 @@ export const certificates = [
     category: "Course",
     image: "/Certificates/Reconnaissance-Enumeration.jpg",
   },
+  {
+    title: "Introduction to AI-Assisted Vibe Coding ",
+    issuer: "Kerala Startup Mission",
+    year: 2026,
+    category: "Webinar",
+    image: "/Certificates/vibe coding.jpg",
+  },
    {
     title: "Figma Essentials For Modern Design",
     issuer: "Kerala Startup Mission",
